71154c665a18b6b1633e992f1ef51b294d18e930
GreatWayne8 <seanwayne3355@gmail.com>
2024-08-15 07:27:14 +0300

    settings modification

--- config/settings.py
+++ config/settings.py
@@ -25,9 +25,9 @@ SECRET_KEY = config(
 )
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config("DEBUG", default=True, cast=bool)
+DEBUG = config("DEBUG", default=False, cast=bool)
 
-ALLOWED_HOSTS = ["127.0.0.1", "your-domain.com"]
+ALLOWED_HOSTS = ["127.0.0.1", "*","your-domain.com"]
 
 # change the default user models to our custom model
 AUTH_USER_MODEL = "accounts.User"
